19c183542d3dcd80d76323f51f8ba26caeae8374
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-30 12:35:30 -0700

    wrote test for signs in a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -19,5 +21,14 @@ describe('top-secrets routes', () => {
       .send(newUser);
     
     expect(res.body).toEqual({ id: expect.any(String), email: 'clare@gmail.com' });
+  });
+
+  it('signs in a user with a POST', async () => {
+    const user = await UserService.create({ email: 'clare@gmail.com', password: 'secretpassword' });
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(user);
+    
+    expect(res.body).toEqual({ message: 'Signed in successfully!', user });
   });
 });
