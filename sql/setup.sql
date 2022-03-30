c54869e461534fdf532dbcd9c5c055bf72b0128b
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-29 17:58:07 -0700

    got rid of qsl keyword as table name

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS secret CASCADE;
 
-CREATE TABLE user (
+CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
     password_hash TEXT NOT NULL
-)
+);
 
 CREATE TABLE secret (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    created_at TIMESTAMP WITH THE TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
